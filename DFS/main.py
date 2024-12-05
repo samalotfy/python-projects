91452bb2f97822220db25b7fe080f516261dba64
samalotfy <samaahmedlotfyy@gmail.com>
2024-12-05 23:11:51 +0200

    Create main.py

---
+++ DFS/main.py
@@ -0,0 +1,21 @@
+def dfs(graph,start,goal):
+	visited=[] 
+	stack=[[start]]
+	while stack: 
+		path=stack.pop()
+
+		node=path[-1] 
+		if node in visited: 
+			continue 
+		visited.append(node) 
+		if node==goal: 
+			return path 
+		else:
+			adjacentnodes=graph.get(node,[]) 
+			for node2 in adjacentnodes:
+				newpath=path.copy() 
+				newpath.append(node2)
+				stack.append(newpath) 
+
+solution2=dfs(graph,'S','G') 
+print('the solution of dfs is ' , solution2)
